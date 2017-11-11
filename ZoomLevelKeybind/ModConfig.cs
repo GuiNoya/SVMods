7a7583b9a0ad257d344c3f1a935f70ffe94a4a52
Guilherme Noya <gui.noya@gmail.com>
2017-11-11 18:48:29 -0200

    Add controller support and increased zoom limits to Zoom Level Keybind

--- ZoomLevelKeybind/ModConfig.cs
+++ ZoomLevelKeybind/ModConfig.cs
@@ -4,8 +4,12 @@ namespace ZoomLevelKeybind
 {
     internal class ModConfig
     {
-        public string IncreaseZoom { get; set; } = Keys.OemPeriod.ToString();
-        public string DecreaseZoon { get; set; } = Keys.OemComma.ToString();
+        public string IncreaseZoomKey { get; set; } = Keys.OemPeriod.ToString();
+        public string DecreaseZoomKey { get; set; } = Keys.OemComma.ToString();
+        public string IncreaseZoomButton { get; set; } = Buttons.LeftStick.ToString();
+        public string DecreaseZoomButton { get; set; } = Buttons.RightStick.ToString();
+        public bool SuppressControllerButton { get; set; } = true;
+        public bool MoreZoom { get; set; } = true;
         public bool UnlimitedZoom { get; set; } = false;
     }
 }
--- ZoomLevelKeybind/ModEntry.cs
+++ ZoomLevelKeybind/ModEntry.cs
@@ -1,6 +1,8 @@
-﻿using StardewModdingAPI;
+﻿using System;
+using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
+using Microsoft.Xna.Framework.Input;
 
 namespace ZoomLevelKeybind
 {
@@ -10,33 +12,64 @@ namespace ZoomLevelKeybind
         public override void Entry(IModHelper helper)
         {
             config = helper.ReadConfig<ModConfig>();
-
-            ControlEvents.KeyPressed += ControlEvents_KeyPressed;
+            
+            InputEvents.ButtonPressed += InputEvents_ButtonPressed;
         }
 
-        private void ControlEvents_KeyPressed(object sender, EventArgsKeyPressed e)
+        private void InputEvents_ButtonPressed(object sender, EventArgsInput e)
         {
             //if (!Context.IsWorldReady)
             //    return;
 
-            if (e.KeyPressed.ToString() == config.IncreaseZoom)
+            if (e.Button.TryGetKeyboard(out Keys key))
             {
-                if (!config.UnlimitedZoom && Game1.options.zoomLevel >= 1.25f)
-                {
-                    Game1.options.zoomLevel = 1.25f;
-                    return;
-                }
-                Game1.options.zoomLevel += 0.05f;
+                if (key.ToString() == config.IncreaseZoomKey)
+                    IncreaseZoom();
+                else if (key.ToString() == config.DecreaseZoomKey)
+                    DecreaseZoom();
+                return;
             }
-            else if (e.KeyPressed.ToString() == config.DecreaseZoon)
+
+            if (e.Button.TryGetController(out Buttons button))
             {
-                if (!config.UnlimitedZoom && Game1.options.zoomLevel <= 0.75f)
+                bool wasZoom = false;
+
+                if (button.ToString() == config.IncreaseZoomButton)
                 {
-                    Game1.options.zoomLevel = 0.75f;
-                    return;
+                    IncreaseZoom();
+                    wasZoom = true;
                 }
-                Game1.options.zoomLevel -= 0.05f;
+                else if (button.ToString() == config.DecreaseZoomButton)
+                {
+                    DecreaseZoom();
+                    wasZoom = true;
+                }
+
+                if (config.SuppressControllerButton && wasZoom)
+                    e.SuppressButton();
             }
+        }
+
+        private void IncreaseZoom()
+        {
+            if (config.UnlimitedZoom)
+                Game1.options.zoomLevel = Game1.options.zoomLevel >= 0.05f ? 0.05f : (float)Math.Round((double)Game1.options.zoomLevel + 0.05, 2);
+            else
+                Game1.options.zoomLevel = Game1.options.zoomLevel >= 1.25f ? 1.25f : (float)Math.Round((double)Game1.options.zoomLevel + 0.05, 2);
+
+            Program.gamePtr.refreshWindowSettings();
+        }
+
+        private void DecreaseZoom()
+        {
+            if (config.UnlimitedZoom)
+                Game1.options.zoomLevel = Game1.options.zoomLevel <= 0.05f ? 0.05f : (float)Math.Round((double)Game1.options.zoomLevel - 0.05, 2);
+            else if (config.MoreZoom)
+                Game1.options.zoomLevel = Game1.options.zoomLevel <= 0.35f ? 0.35f : (float)Math.Round((double)Game1.options.zoomLevel - 0.05, 2);
+            else
+                Game1.options.zoomLevel = Game1.options.zoomLevel <= 0.75f ? 0.75f : (float)Math.Round((double)Game1.options.zoomLevel - 0.05, 2);
+
+            Program.gamePtr.refreshWindowSettings();
         }
     }
 }
