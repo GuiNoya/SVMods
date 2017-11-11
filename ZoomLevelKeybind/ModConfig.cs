a8c1b953f0180745bb35d5013b2e7736c7e8d3f0
Guilherme Noya <gui.noya@gmail.com>
2017-11-11 19:23:24 -0200

    Change controller bindings

--- ZoomLevelKeybind/ModConfig.cs
+++ ZoomLevelKeybind/ModConfig.cs
@@ -6,8 +6,8 @@ namespace ZoomLevelKeybind
     {
         public string IncreaseZoomKey { get; set; } = Keys.OemPeriod.ToString();
         public string DecreaseZoomKey { get; set; } = Keys.OemComma.ToString();
-        public string IncreaseZoomButton { get; set; } = Buttons.LeftStick.ToString();
-        public string DecreaseZoomButton { get; set; } = Buttons.RightStick.ToString();
+        public string IncreaseZoomButton { get; set; } = Buttons.RightStick.ToString();
+        public string DecreaseZoomButton { get; set; } = Buttons.LeftStick.ToString();
         public bool SuppressControllerButton { get; set; } = true;
         public bool MoreZoom { get; set; } = true;
         public bool UnlimitedZoom { get; set; } = false;
